eb02f4462b22739de6bc0522b0b0a89e978fa16e
Jeff Kwon <jykwon007@gmail.com>
2024-05-04 22:06:40 -0700

    Add class 'operand' to elements that require

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 // const buttonSeven = document.querySelector('7')
 const operandButton = document.querySelectorAll('.operand')
+const display = document.querySelector('.display')
+
+let currentDisplay = 0
 
 function handleClick(e) {
-  console.log(e.target)
+  console.log(e.target.value)
+  display.textContent = e.target.value
 }
 console.log(operandButton)
 // operandButton.addEventListener('click', handleClick)
+
+operandButton.forEach(item => {
+  item.addEventListener('click', handleClick)
+})
