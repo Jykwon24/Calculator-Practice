98fbe76f630cf43b80a952d6a798624dd9f87144
Jeff Kwon <jykwon007@gmail.com>
2024-05-04 21:43:25 -0700

    Add new main html element, start JS implementation

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello')
+// const buttonSeven = document.querySelector('7')
+const operandButton = document.querySelectorAll('.operand')
+
+function handleClick(e) {
+  console.log(e.target)
+}
+console.log(operandButton)
+// operandButton.addEventListener('click', handleClick)
