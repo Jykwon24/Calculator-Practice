5ca48d431e8cfc23548a40a719c266b0cdb89e6b
Jeff Kwon <jykwon007@gmail.com>
2024-05-07 16:06:35 -0700

    Update value to string, so user is able to enter digits past the tens

--- index.js
+++ index.js
@@ -2,42 +2,32 @@
 const main = document.querySelector('main')
 const operandButton = document.querySelectorAll('.operand')
 const operatorButton = document.querySelectorAll('.operator')
-const display = document.querySelector('.display')
+const value = document.querySelector('.display')
 
-let currentValue = 0
+let currentValue = ''
 let valuesArr = []
 let currentOperation = ''
-console.log(currentDisplay)
-console.log(currentOperation)
-
 
 function handleClick(event) {
   const targetEvent = event.target.className
   switch (targetEvent) {
     case 'operand':
-      updateDisplay(event)
+      updateValue(event)
       break;
     case 'operator':
       selectOperator(event)
       break;
 
   }
-
-
-  console.log('inside handleclick', currentValue)
+  console.log('Value inside handleclick', currentValue)
+  console.log('Operator inside handleclick:', currentOperation)
 }
-// console.log(operandButton)
-// operandButton.addEventListener('click', handleClick)
-
-// main.forEach(item => {
-//   item.addEventListener('click', handleClick)
-// })
 
 main.addEventListener('click', handleClick)
 
-function updateDisplay(num) {
-  currentDisplay = num.target.value
-  display.textContent = currentValue
+function updateValue(num) {
+  currentValue += num.target.value
+  value.textContent = currentValue
 }
 
 function containValues() {
@@ -48,8 +38,6 @@ function containValues() {
 function selectOperator(event) {
   switch (event.target.value) {
     case '+':
-      // console.log(event.target.value)
-      // console.log(currentDisplay)
       currentOperation = event.target.value
       break;
     case '=':
