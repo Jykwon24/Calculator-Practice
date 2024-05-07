9cee26c9363f85c2b967178282d06b9d4c2f1860
Jeff Kwon <jykwon007@gmail.com>
2024-05-07 15:19:32 -0700

    Update operation and currentvalue on click

--- index.js
+++ index.js
@@ -1,16 +1,65 @@
 // const buttonSeven = document.querySelector('7')
+const main = document.querySelector('main')
 const operandButton = document.querySelectorAll('.operand')
+const operatorButton = document.querySelectorAll('.operator')
 const display = document.querySelector('.display')
 
-let currentDisplay = 0
+let currentValue = 0
+let valuesArr = []
+let currentOperation = ''
+console.log(currentDisplay)
+console.log(currentOperation)
 
-function handleClick(e) {
-  console.log(e.target.value)
-  display.textContent = e.target.value
+
+function handleClick(event) {
+  const targetEvent = event.target.className
+  switch (targetEvent) {
+    case 'operand':
+      updateDisplay(event)
+      break;
+    case 'operator':
+      selectOperator(event)
+      break;
+
+  }
+
+
+  console.log('inside handleclick', currentValue)
 }
-console.log(operandButton)
+// console.log(operandButton)
 // operandButton.addEventListener('click', handleClick)
 
-operandButton.forEach(item => {
-  item.addEventListener('click', handleClick)
-})
+// main.forEach(item => {
+//   item.addEventListener('click', handleClick)
+// })
+
+main.addEventListener('click', handleClick)
+
+function updateDisplay(num) {
+  currentDisplay = num.target.value
+  display.textContent = currentValue
+}
+
+function containValues() {
+  valuesArr.push(currentValue)
+  console.log(valuesArr)
+}
+
+function selectOperator(event) {
+  switch (event.target.value) {
+    case '+':
+      // console.log(event.target.value)
+      // console.log(currentDisplay)
+      currentOperation = event.target.value
+      break;
+    case '=':
+      compute()
+      break;
+  }
+}
+
+
+
+function compute() {
+  console.log(currentOperation)
+}
